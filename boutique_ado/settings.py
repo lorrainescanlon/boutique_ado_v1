c5b9b5bc4a1b77f5c7f0fc5b25ae5d45199379cc
lorrainescanlon <lorrainescanlon@gmail.com>
2025-04-25 21:33:32 +0100

    added toast messaging, shopping bag preview and notifications

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICCATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of 'allauth'
     'django.contrib.auth.backends.ModelBackend',
