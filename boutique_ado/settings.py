f83df11be0425c74e6a9b230239494847d91fe5c
lorrainescanlon <lorrainescanlon@gmail.com>
2025-05-07 01:47:47 +0100

    update email settings typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -225,7 +225,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = TrueEMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
